ea9569f05b94a54d5b73c68249b9405381bd5d7f
Karuppiah Natarajan <karuppiah7890@gmail.com>
2019-07-19 20:59:37 +0530

    read two arguments from command line - food file and version number

--- main.go
+++ main.go
@@ -9,11 +9,18 @@ import (
 
 func main() {
 	app := &cli.App{
-		Name: "uff",
-		Usage: "update (go)fish food",
+		Name:      "uff",
+		Usage:     "update (go)fish food",
 		UsageText: "uff <food-file> <version>",
 		Action: func(c *cli.Context) error {
-			fmt.Println("upgrading fish food ...")
+			if c.NArg() != 2 {
+				fmt.Println("pass exactly two arguments\n")
+				cli.ShowAppHelpAndExit(c, 1)
+				return nil
+			}
+			foodFile := c.Args().Get(0)
+			version := c.Args().Get(1)
+			fmt.Printf("upgrading fish food %s to version %s ...\n", foodFile, version)
 			return nil
 		},
 	}
