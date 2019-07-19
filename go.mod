b74ebdd0d3b20531917fa09362a80582066bcbdc
Karuppiah Natarajan <karuppiah7890@gmail.com>
2019-07-19 20:43:44 +0530

    create a dummy command line app

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/karuppiah7890/uff
 
 go 1.12
+
+require gopkg.in/urfave/cli.v2 v2.0.0-20180128182452-d3ae77c26ac8
---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"gopkg.in/urfave/cli.v2"
+)
+
+func main() {
+	app := &cli.App{
+		Name: "uff",
+		Usage: "update (go)fish food",
+		Action: func(c *cli.Context) error {
+			fmt.Println("upgrading fish food ...")
+			return nil
+		},
+	}
+
+	app.Run(os.Args)
+}
